fe70ba446c41abbdce79db1f92ae35fcca75a937
jefflongo <jeff.a.longo@gmail.com>
2020-03-11 18:05:07 -0700

    Finish Hello Triangle

--- src/Renderer.cpp
+++ src/Renderer.cpp
@@ -2,18 +2,48 @@
 
 #include <iostream>
 
+const char* vertex_shader_source =
+  "#version 330 core\n"
+  "layout (location = 0) in vec3 pos;\n"
+  "void main()\n"
+  "{\n"
+  "   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);\n"
+  "}\0";
+
+const char* fragment_shader_source =
+  "#version 330 core\n"
+  "out vec4 FragColor;\n"
+  "void main()\n"
+  "{\n"
+  "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+  "}\n\0";
+
+float vertices[] = {
+    0.5f,  0.5f,  0.0f, // top right
+    0.5f,  -0.5f, 0.0f, // bottom right
+    -0.5f, -0.5f, 0.0f, // bottom left
+    -0.5f, 0.5f,  0.0f  // top left
+};
+
+unsigned int indices[] = {
+    0, 1, 3, // first triangle
+    1, 2, 3  // second triangle
+};
+
 Renderer::Renderer(std::string name, int width, int height) {
     glfwInit();
     // make sure the opengl version is 3.3
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    // glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
+    // generate window
     window = glfwCreateWindow(width, height, name.c_str(), NULL, NULL);
     if (!window) {
         std::cout << "Failed to create GLFW window" << std::endl;
-        glfwTerminate();
         exit(1);
     }
     glfwMakeContextCurrent(window);
@@ -24,9 +54,36 @@ Renderer::Renderer(std::string name, int width, int height) {
         exit(1);
     }
 
-    // set the lower left corner and the upper right corner
-    // opengl transforms the 2d coords to coordinates on the screen.
-    glViewport(0, 0, width, height);
+    if (!generate_shaders()) {
+        exit(1);
+    }
+
+    // generate gl object for vertex array (attributes)
+    glGenVertexArrays(1, &vao);
+    // generate gl object for vertex buffer (data)
+    glGenBuffers(1, &vbo);
+    // generate gl object for element buffer (unique indices)
+    glGenBuffers(1, &ebo);
+    // bind vertex array object (to save vertex configuration)
+    glBindVertexArray(vao);
+    // bind vertex buffer object (buffer for data)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    // bind element buffer object (buffer for unique indices)
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    // copy data from vbo buffer to GPU
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    // copy data from ebo buffer to GPU
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    // Configure vertex attributes (will be saved to vao)
+    // * location 0
+    // * vertex structure contains 3 items (x, y, z)
+    // * vertex structure uses floating point
+    // * unnormalized (only needed for integer type)
+    // * size of vertex structure
+    // * offset 0 bytes into buffer
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // enable location 0 vertex attributes
+    glEnableVertexAttribArray(0);
 
     // register the callback functions
     glfwSetFramebufferSizeCallback(window, resize_callback);
@@ -48,8 +105,58 @@ Renderer::~Renderer() {
 }
 
 void Renderer::render() {
-    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // Clear screen
+    glClearColor(0, 0, 0, 1);
     glClear(GL_COLOR_BUFFER_BIT);
+
+    // Load shaders
+    glUseProgram(shader);
+    // Load vertex attribute configuration
+    glBindVertexArray(vao);
+    // Draw contents of VBO (bound from VAO)
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+}
+
+bool Renderer::generate_shaders() {
+    int success;
+    char log[512];
+    // compile our custom vertex shader
+    int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex_shader, 1, &vertex_shader_source, NULL);
+    glCompileShader(vertex_shader);
+    glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertex_shader, sizeof(log), NULL, log);
+        std::cout << "Failed to compile vertex shader\n:" << log << std::endl;
+        return false;
+    }
+
+    // compile our custom fragment shader
+    int fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment_shader, 1, &fragment_shader_source, NULL);
+    glCompileShader(fragment_shader);
+    glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragment_shader, sizeof(log), NULL, log);
+        std::cout << "Failed to compile vertex shader:\n" << log << std::endl;
+        return false;
+    }
+
+    // link our custom shaders
+    shader = glCreateProgram();
+    glAttachShader(shader, vertex_shader);
+    glAttachShader(shader, fragment_shader);
+    glLinkProgram(shader);
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shader, sizeof(log), NULL, log);
+        std::cout << "Failed to link shaders:\n" << log << std::endl;
+        return false;
+    }
+    glDeleteShader(vertex_shader);
+    glDeleteShader(fragment_shader);
+
+    return true;
 }
 
 void Renderer::resize_callback(GLFWwindow* window, int width, int height) {
--- src/Renderer.h
+++ src/Renderer.h
@@ -9,15 +9,28 @@
 
 class Renderer {
   public:
+    // Initialize window named name, with dimensions, width x height
     Renderer(std::string name, int width, int height);
+    // Clean up GLFW allocation
     ~Renderer(void);
 
+    // Render loop. Will block until exit condition
     void loop(void);
 
   private:
+    // Rendering pipeline, called once per render loop
     void render(void);
+    // Compiles and generates shaders for initialization
+    bool generate_shaders(void);
+
+    // GLFW callbacks
     static void resize_callback(GLFWwindow* window, int width, int height);
     static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
 
+    // Render window
     GLFWwindow* window;
+    // IDs reserved for storing data to GPU
+    unsigned int vao, vbo, ebo;
+    // ID for shader program object
+    int shader;
 };
